338c8105711838927bbec8bd8aca94dbf73e3773
furukawa <dkszoeke@outlook.de>
2026-08-21 16:53:01 +0200

    feat: added custom command to push ocr-data into testing database
    So that I dont have false commits in the actual database and need to remove them via SQL queries

--- src/beutelnet/bagsearch/management/commands/push_new_data.py
+++ src/beutelnet/bagsearch/management/commands/push_new_data.py
@@ -1,11 +1,22 @@
 from django.core.management.base import BaseCommand, CommandError
 from bagsearch.models import VacuumBags
 
-from bagsearch.pushdata import pushdata
+from bagsearch.pushdata import push_new_OCR_data 
+from bagsearch.pushdata import test_push_OCR_data 
 
 class Command(BaseCommand):
     help="Pushing newly processed image text into database."
 
+    def add_arguments(self, parser):
+        parser.add_argument("--test",
+                            action="store_true",
+                            help="push data into test database bagsearch_testbags"
+                            )
+
+
     def handle(self, *args, **kwargs):
-        push_new_OCR_data()
-        print("Inserted new data.")
+        if kwargs["test"]:
+            test_push_OCR_data()
+        else:
+            push_new_OCR_data()
+            print("Inserted new data.")
